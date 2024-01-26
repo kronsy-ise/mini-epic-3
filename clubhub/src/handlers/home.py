9a61c0d596e697138857a0b6d2e29bab6d31cdb1
Samir Bioud <samir@kronsy.dev>
2024-01-26 20:16:47 +0000

    Feat: Add Signup, Fix: References to old is_admin field

--- clubhub/src/handlers/home.py
+++ clubhub/src/handlers/home.py
@@ -3,12 +3,10 @@ from flask import Blueprint, make_response, request, render_template, session, r
 from globals import db
 from models.user import User
 from models.user import UserKind
-import bcrypt
 import psycopg2.errors as pgerrors
-import random
-import datetime
 import util
 from models.user import User
+import os
 
 home_app = Blueprint('home_app', __name__)
 
@@ -25,7 +23,7 @@ def homepage():
     # Depending on the type of registered user 
     # we show a different home page
     
-    if auth_user.is_admin:
+    if auth_user.kind == UserKind.Admin:
         return render_template("admin/home.html")
     elif auth_user.kind == UserKind.User:
         return render_template("user/home.html")
